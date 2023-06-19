f6d9fbb2e7ebf20f48e0e6620c9ff31eb3f10714
David Jenni <me@davidrjenni.io>
2023-06-19 19:50:52 +0200

    lexer: add Tok type

---
+++ lexer/tok.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2023 David Jenni. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package lexer // import "davidrjenni.io/lang/lexer"
+
+// Tok represents the set of lexical tokens.
+type Tok int
+
+//go:generate stringer -type=Tok -linecomment
+
+const (
+	// EOF marks the end of file.
+	EOF     Tok = iota // EOF
+	Illegal            // illegal
+)
