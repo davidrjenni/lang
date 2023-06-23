90fc775aa673b087f24300856c525744c7f46348
David Jenni <me@davidrjenni.io>
2023-06-23 20:08:20 +0200

    types: add scalar types

---
+++ types/types.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2023 David Jenni. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package types // import "davidrjenni.io/lang/types"
+
+import "fmt"
+
+type Type interface {
+	String() string
+	typ()
+}
+
+func Equal(t, u Type) bool {
+	switch t := t.(type) {
+	case *Bool:
+		_, ok := u.(*Bool)
+		return ok
+	case *I64:
+		_, ok := u.(*I64)
+		return ok
+	case *F64:
+		_, ok := u.(*F64)
+		return ok
+	case *String:
+		_, ok := u.(*String)
+		return ok
+	default:
+		panic(fmt.Sprintf("unexpected type %T", t))
+	}
+}
+
+type Bool struct{}
+type I64 struct{}
+type F64 struct{}
+type String struct{}
+
+func (*Bool) String() string   { return "bool" }
+func (*I64) String() string    { return "i64" }
+func (*F64) String() string    { return "f64" }
+func (*String) String() string { return "string" }
+
+func (*Bool) typ()   {}
+func (*I64) typ()    {}
+func (*F64) typ()    {}
+func (*String) typ() {}
---
+++ types/types_test.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2023 David Jenni. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package types_test
+
+import (
+	"testing"
+
+	"davidrjenni.io/lang/types"
+)
+
+func TestEqual(t *testing.T) {
+	tests := [...]struct {
+		t, u     types.Type
+		expected bool
+	}{
+		{t: &types.Bool{}, u: &types.Bool{}, expected: true},
+		{t: &types.I64{}, u: &types.I64{}, expected: true},
+		{t: &types.F64{}, u: &types.F64{}, expected: true},
+		{t: &types.String{}, u: &types.String{}, expected: true},
+
+		{t: &types.Bool{}, u: &types.I64{}, expected: false},
+		{t: &types.Bool{}, u: &types.F64{}, expected: false},
+		{t: &types.Bool{}, u: &types.String{}, expected: false},
+
+		{t: &types.I64{}, u: &types.Bool{}, expected: false},
+		{t: &types.I64{}, u: &types.F64{}, expected: false},
+		{t: &types.I64{}, u: &types.String{}, expected: false},
+
+		{t: &types.F64{}, u: &types.Bool{}, expected: false},
+		{t: &types.F64{}, u: &types.I64{}, expected: false},
+		{t: &types.F64{}, u: &types.String{}, expected: false},
+
+		{t: &types.String{}, u: &types.Bool{}, expected: false},
+		{t: &types.String{}, u: &types.I64{}, expected: false},
+		{t: &types.String{}, u: &types.F64{}, expected: false},
+	}
+
+	for i, test := range tests {
+		actual := types.Equal(test.t, test.u)
+		if actual != test.expected {
+			t.Errorf("%d: expected %s to be equal to %s", i, test.t, test.u)
+		}
+	}
+}
